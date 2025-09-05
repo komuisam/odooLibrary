e560d77824f5d58010b80a3075be8d81f191bafc
carlos <komuisam@gmail.com>
2025-09-05 09:12:03 -0400

    [ADD] agregando - addons

---
+++ addons/library_member/models/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class library_member(models.Model):
+    _name = 'library_member.library_member'
+    _description = 'library_member.library_member'
+
+    title = fields.Char()
+    count = fields.Integer()
+    value = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
+
